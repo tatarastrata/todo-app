19fea166542a3a3b9e1a202cdad7a43ebc4e33db
tamara ostapets <tamara.ostapec@gmail.com>
2021-02-08 18:32:34 +0200

    initial commit

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Todo } from '../Todo';
+import PropTypes from 'prop-types';
+import { TypeTodo } from '../../types';
+
+export const TodoList = ({
+  removeItem,
+  todos,
+  checkTodo,
+  handleEditingTodo,
+  handleEditedTodo,
+}) => (
+  <ul className="todo-list">
+    {todos.map(todo => (
+      <Todo
+        key={todo.id}
+        removeItem={removeItem}
+        checkTodo={checkTodo}
+        handleEditingTodo={handleEditingTodo}
+        handleEditedTodo={handleEditedTodo}
+        todo={todo}
+      />
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  removeItem: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(TypeTodo).isRequired,
+  checkTodo: PropTypes.func.isRequired,
+  handleEditingTodo: PropTypes.func.isRequired,
+  handleEditedTodo: PropTypes.func.isRequired,
+};
